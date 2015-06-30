45a1e89933912f8ce68d4ea2c729e119a93c07fb
leocostaba <leocostaba1@gmail.com>
2015-06-30 17:47:33 -0300

    calling css and js files

--- sobre.php
+++ sobre.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Home - SEMCOMP 2015</title>
+    <title>O Evento - SEMCOMP 2015</title>
     <?php require_once('includes/head.inc');?>
 </head>
 <body>
    <?php require_once('includes/navigation.inc');?>
+   div
+
 </body>
 </html>
