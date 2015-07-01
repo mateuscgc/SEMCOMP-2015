344e31755a75e6f407915dfac1f3ffd2d23182cc
Mateus Cordeiro <mateus_cgc@hotmail.com>
2015-07-01 00:15:41 -0300

    Iniciando Home

--- index.php
+++ index.php
@@ -7,5 +7,16 @@
 </head>
 <body>
    <?php require_once('includes/navigation.inc');?>
+   <header class="home-header">
+       <video class="home-bg-video" autoplay loop poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg">
+           <source src="http://demosthenes.info/assets/videos/polina.webm" type="video/webm">
+           <source src="http://demosthenes.info/assets/videos/polina.mp4" type="video/mp4">
+       </video>
+       <div class="social-networks-home">
+           <i class="icon-facebook-sign "></i>
+           <i class="icon-facebook-sign"></i>
+           <i class="icon-facebook-sign"></i>
+       </div>
+   </header>
 </body>
 </html>
