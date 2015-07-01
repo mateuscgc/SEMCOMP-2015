2319b76d5f19beb25176d7c0ab885b2b0a5d4418
Mateus Cordeiro <mateus_cgc@hotmail.com>
2015-07-01 18:52:14 -0300

    Iniciado Parte de Keynotes
    Refs #10

--- index.php
+++ index.php
@@ -17,8 +17,70 @@
                 <a href="https://www.youtube.com/user/SEMCOMPUFBA" target="_blank"><i class="icon-youtube"></i></a>
                 <a ><i class="icon-envelope"></i></a>
             </div>
+            <div class="intro-wrapper">
+                <span>Apresentamos a 5ª Edição da Maior Semana da Computação do Brasil</span>
+                <h1>SEMCOMP <span>2015</span></h1>
+                <h2>02 a 05 de Setembro</h2>
+                <span>na Escola Politécnica da UFBA, Salvador</span>
+            </div>
         </div>
-
     </header>
+    <main>
+        <section class="regular-section" id="keynotes">
+            <div class="content">
+                <div class="row">
+                    <div class="keynote col-md-4 col-sm-6">
+                        <img src="assets/images/caio-sacramento.jpg" height="960" width="720" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">Caio Sacramento</h2>
+                            <span class="keynote-title">Trainee</span>
+                        </div>
+                    </div>
+                    <div class="keynote col-md-4 col-sm-6">
+                        <img src="assets/images/cristiano-santos.jpg" height="222" width="222" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">Cristiano Santos</h2>
+                            <span class="keynote-title">Pós Júnior</span>
+                        </div>
+                    </div>
+                    <div class="keynote col-md-4 col-sm-6">
+                        <img src="assets/images/diego-cajaiba.jpg" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">Diego Cajaiba</h2>
+                            <span class="keynote-title">Assessor de Presidência</span>
+                        </div>
+                    </div>
+                    <div class="keynote col-md-4 col-sm-6">
+                        <img src="assets/images/jp.jpg" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">JP</h2>
+                            <span class="keynote-title">Assessor de Marketing</span>
+                        </div>
+                    </div>
+                    <div class="keynote col-md-4 col-sm-6">
+                        <img src="assets/images/kaio-porto.jpg" height="231" width="231" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">Kaio Porto</h2>
+                            <span class="keynote-title">Assessor de Projetos</span>
+                        </div>
+                    </div>
+                    <div class="keynote col-md-4 col-sm-6">
+                        <img src="assets/images/leonardo-costa.jpg" height="960" width="579" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">Leonardo Costa</h2>
+                            <span class="keynote-title">Gerente de Comunicação</span>
+                        </div>
+                    </div>
+                    <div class="keynote col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
+                        <img src="assets/images/vinicius-dias.jpg" height="960" width="960" class="speaker-thumb keynote-thumb"></img>
+                        <div class="keynote-info">
+                            <h2 class="keynote-name">Vinicius Dias</h2>
+                            <span class="keynote-title">Gerete de Treinamentos</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
