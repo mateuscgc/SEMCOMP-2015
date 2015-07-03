f4ec8f004be5ae10036311a74fc6093e8ea56a75
leocostaba <leocostaba1@gmail.com>
2015-07-02 21:35:50 -0300

    Add contact and about us pages #3 #6
    the about us page don't have slideshow in the moment

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Home - SEMCOMP 2015</title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <?php require_once('includes/head.inc');?>
 </head>
 <body>
