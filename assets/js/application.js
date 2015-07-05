caa2e24dd0aeb2ad0cd5781c38a6be685f16480c
Mateus Cordeiro <mateus_cgc@hotmail.com>
2015-07-05 20:16:02 -0300

    Unificando JS

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+    var i = 0;
+    var j = 0;
+    $('#dropdown').click(function() {
+        if (j == 0){
+            $('.programming').slideDown("slow");
+            j++;
+        } else {
+            $('.programming').slideUp("slow");
+            j = 0;
+        }
+    });
+    $('#toggle').click(function() {
+        if (i == 0){
+            $('nav>ul').addClass("show");
+            i++;
+        } else {
+            $('nav>ul').removeClass("show");
+            i = 0;
+        }
+    });
+});
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
