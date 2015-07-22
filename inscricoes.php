aa54055f304fad910d4776b09a92916b02761809
Mateus Cordeiro <mateus_cgc@hotmail.com>
2015-07-21 23:21:57 -0300

    Feita Página de inscrições

---
+++ inscricoes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Minicursos - SEMCOMP 2015</title>
+        <?php require_once('includes/head.inc');?>
+    </head>
+    <body>
+        <?php require_once('includes/navigation.inc');?>
+        <header class="default-header header-tickets">
+        </header>
+        <main>
+            <div class="content overlay-content text-aboutus papers-content">
+                <h3 class="sub-title">Garanta a sua vaga na SEMCOMP <span class="year-color">2015</span></h3>
+                <p>Inscrições para os <b>estudantes da UFBA</b> a partir do dia <b>22/07</b>. As inscrições <b>gerais</b> serão abertas no dia <b>26/07</b>.</p>
+                <ul class="alert">
+                    <li>Lembre-se de levar o documento de indentidade no dia do evento para realizar o credenciamento.</li>
+                    <li>Os estudantes devem levar um documento comprovando vínculo com a instituição de ensino superior.</li>
+                </ul>
+                <!-- Here goes the Doity frame -->
+            </div>
+        </main>
+        <!-- Here goes the Doity script link -->
+        <?php require_once('includes/footer.inc');?>
+    </body>
+</html>
